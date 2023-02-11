d065845fa793a98f634aa8b31943d789b5b1f20c
xokiara <kiaramoncada28@gmail.com>
2023-02-11 10:34:20 -0600

    Ejercicio 9 POO
    POO

---
+++ IParcial/IParcial/Ejercicio9POO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using POO;
+
+namespace IParcial
+{
+    public partial class Ejercicio9POO : Form
+    {
+        public Ejercicio9POO()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Instanciar objetos (crear un objeto de la clase)
+            Coche miCoche = new Coche();
+
+                                        //Marca Modelo
+            Coche miCoche2 = new Coche("Toyota", "22R");
+
+        }
+    }
+}
---
+++ IParcial/POO/Coche.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Coche
+    {
+        //Atributos (siempre serán privados)
+        private string marca;
+        private string modelo;
+        //private decimal precio;
+
+        //Propiedades (son públicas)
+        //Métodos get y set
+        public string Marca 
+        {
+            get { return marca; }  //Sirve para obtener, retorno el valor 
+            set { marca = value; } //sirve para mandarle información
+        }
+
+        public string Modelo { get => modelo; set => modelo = value; }
+
+        //Otra manera más corta y no es necesario declarar los atributos
+        public decimal Precio { get; set; }
+
+        //Se escribe prop y se da tab
+        public int Kilometros { get; set; }
+
+        //Constructores
+        public Coche() { }
+
+                  //Parametros
+        public Coche(string _marca, string _modelo) 
+        { 
+            Marca = _marca;
+            Modelo = _modelo;
+        }
+
+        //Constructor hecha con el tonrnillo que sale a la par, acciones rápidas
+        public Coche(string marca, string modelo, decimal precio, int kilometros) : this(marca, modelo)
+        {
+            Precio = precio;
+            Kilometros = kilometros;
+        }
+
+        //Métodos o acciones
+        public string DevolverDatosBasicos() 
+        {
+            return "Marca: " + Marca + " Modelo: " + Modelo + " Precio: " + Precio;
+        }
+    }
+}
